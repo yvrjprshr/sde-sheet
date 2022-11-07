56f81bfb320146b237dc067c5b03494da4ef5389
Yuvraj Parashar <66880935+yvrjprshr@users.noreply.github.com>
2022-11-07 20:41:33 +0530

    Contains Duplicates

--- dsa.cpp
+++ dsa.cpp
@@ -40,3 +40,20 @@ public:
     }
 };
 
+// Contains Duplicates
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int i = 0; i < nums.size(); i++){
+            if(s.find(nums[i]) == s.end()){
+                s.insert(nums[i]);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
