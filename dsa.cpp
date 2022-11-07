3df0f2e65c598329cbbc842a35b32eee9e39e04b
Yuvraj Parashar <66880935+yvrjprshr@users.noreply.github.com>
2022-11-07 20:29:33 +0530

    two sum

--- dsa.cpp
+++ dsa.cpp
@@ -1,3 +1,27 @@
 // Blind-75
 
-// Contains Duplicates
+/*------------------------------------------ Array -----------------------------------------------------*/
+
+
+// Two Sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ump;
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++){
+            int required = target-nums[i];
+            if(ump.find(required) != ump.end()){
+                result.push_back(i);
+                result.push_back(ump[required]);
+                break;
+            }else{
+                ump[nums[i]] = i;
+            }
+        }
+        return result;
+    }
+};
+
