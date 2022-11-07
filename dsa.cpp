3b673f07d6f1b7a64e4f68bafdf4d8f224ee187d
Yuvraj Parashar <66880935+yvrjprshr@users.noreply.github.com>
2022-11-07 20:34:12 +0530

    Best Time to buy and sell stock

--- dsa.cpp
+++ dsa.cpp
@@ -25,3 +25,18 @@ public:
     }
 };
 
+// Best Time to buy and sell stock
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int buy=prices[0];
+        for(int i=1;i<prices.size();i++){
+            profit=max(profit,prices[i]-buy);
+            buy=min(buy,prices[i]);
+        }
+        return profit;
+    }
+};
+
